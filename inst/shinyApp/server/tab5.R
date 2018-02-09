89a5f0d7ae415d22bd71941072ac1e46d31eefd8
bofeeee <8196385@qq.com>
2018-02-09 15:44:58 -0500

    fix Tab 5 error

--- inst/shinyApp/server/tab5.R
+++ inst/shinyApp/server/tab5.R
@@ -113,72 +113,37 @@ observe({
   
 })
 
-data_mul_name_tab5 <- eventReactive(input$sub_mul_tab5,{
-  RaMP:::rampFastOntoFromMeta(input$input_mul_tab5,
-                              conpass = .conpass,
-                              host =.host,
-                              username = .username,
-                              dbname = .dbname,
-                              sourceOrName ='name')
+data_mul_tab5 <- eventReactive(input$sub_mul_tab5,{
+  if(input$input_categories_tab5 %in% c('source','name')){
+    RaMP:::rampFastOntoFromMeta(input$input_mul_tab5,
+                                conpass = .conpass,
+                                host =.host,
+                                username = .username,
+                                dbname = .dbname,
+                                sourceOrName =input$input_categories_tab5)
+  } else if(input$input_categories_tab5 == 'ontology'){
+    RaMP:::rampFastMetaFromOnto(input$input_mul_tab5,
+                                conpass = .conpass,
+                                host = .host,
+                                username = .username,
+                                dbname = .dbname)
+  }
 })
-data_mul_source_tab5 <- eventReactive(input$sub_mul_tab5_sourceid,{
-  RaMP:::rampFastOntoFromMeta(input$input_mul_tab5_sourceid,
-                              conpass = .conpass,
-                              host =.host,
-                              username = .username,
-                              dbname = .dbname,
-                              sourceOrName ='source')
-})
-data_mul_biofluid_tab5 <- eventReactive(input$sub_mul_tab5_biofluid,{
-  RaMP:::rampFastMetaFromOnto(input$input_mul_tab5_biofluid,
-                              conpass = .conpass,
-                              host =.host,
-                              username = .username,
-                              dbname = .dbname
-                              )
-})
-# data_mul_file_tab5 <- eventReactive(input$sub_file_tab5,{
-#   infile <- input$inp_file_tab5
-#   if (is.null(infile))
-#     return(NULL)
-#   
-#   rampOut <- rampFileOfBiofluid(infile)
-# })
-# observe({
-#   input$sub_file_tab5
-#   
-#   detector_tab5$num <- 2
-# })
 
 output$preview_multi_names_tab5 <- DT::renderDataTable({
   if(is.null(clicked$content))
     return(NULL)
-  if(clicked$content == 'source'){
-    data_mul_source_tab5()
-  } else if (clicked$content == 'name'){
-    data_mul_name_tab5()
-  } else if (clicked$content == 'ontology' ){
-    data_mul_biofluid_tab5()
-  }
+  data_mul_tab5()
 },rownames = F)
 
 output$tab5_mul_report <- downloadHandler(filename = function() {
-  if(detector_tab5$num == 1){
-    paste0("textAreaInput", ".csv")
-  } else if (detector_tab5$num == 2) {
-    paste0("fileInput",".csv")
-  }
-}, content = function(file) {
-  if(clicked$content == 'name'){
-    rampout <- data_mul_name_tab5()
-  } else if (clicked$content == 'source'){
-    rampout <- data_mul_source_tab5() 
-    
-  } else if (clicked$conttent == 'ontology') {
-    rampout <- data_mul_biofluid_tab5()
-  } else {
+  if(is.null(clicked$content))
     return(NULL)
-  }
+  return(paste0(input$input_categories_tab5,'report.csv'))
+}, content = function(file) {
+  if(is.null(clicked$content))
+    return(NULL)
+  rampout <- data_mul_tab5()
   
   write.csv(rampout, file, row.names = FALSE)
 })
