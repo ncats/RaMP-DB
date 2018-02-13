e939d46dec59765cba1e2da6227ecd6dca836768
andyptt21 <pat271@ISYS-NC062546.local>
2018-02-12 19:50:12 -0500

    Fixed issues with combination pathway analysis

--- R/GeneralFunctions.R
+++ R/GeneralFunctions.R
@@ -285,8 +285,9 @@ killDbConnections <- function() {
 #` Load pathway overlap matrices for find_clusters function
 #`
 load_overlap_matrices<- function() {
-  gene_result <- metabolite_result <- c()
+  gene_result <- metabolite_result <- analyte_result <- c()
   load(system.file(package = "RaMP",... = "extdata/genes_overlap_matrix.RData"))
   load(system.file(package = "RaMP",... = "extdata/metabolites_overlap_matrix.RData"))
-  return(list(gene_result,metabolite_result))
+  load(system.file(package = "RaMP",... = "extdata/analytes_overlap_matrix.RData"))
+  return(list(gene_result,metabolite_result,analyte_result))
 }
