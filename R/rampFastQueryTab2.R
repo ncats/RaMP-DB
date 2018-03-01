cda189f798a53efda583c3927c63c7851f7b532e
bofeeee <8196385@qq.com>
2018-03-01 11:43:09 -0500

    fix issue 4

--- R/rampFastQueryTab2.R
+++ R/rampFastQueryTab2.R
@@ -13,12 +13,12 @@
 #' }
 #' @export
 getAnalyteFromPathway <- function(pathway,conpass=NULL,
-	dbname="ramp",username="root",host = "localhost"){
-
+                                  dbname="ramp",username="root",host = "localhost"){
+  
   if(is.null(conpass)) {
-        stop("Please define the password for the mysql connection")
+    stop("Please define the password for the mysql connection")
   }
-
+  
   now <- proc.time()
   print("fired")
   if(is.character(pathway)){
@@ -43,13 +43,13 @@ getAnalyteFromPathway <- function(pathway,conpass=NULL,
                        host = host)
   query1 <- paste0("select * from pathway where pathwayName
                    in (",list_pathway,");")
-
+  
   df1 <- DBI::dbGetQuery(con,query1)
   DBI::dbDisconnect(con)
-
+  
   if(nrow(df1)==0) {
-	stop("None of the input pathway(s) could be found")}
-
+    stop("None of the input pathway(s) could be found")}
+  
   # Retrieve compound id from RaMP pathway id (query1)
   query2 <- paste0("select pathwayRampId,rampId from analytehaspathway where
                    pathwayRampId in (select pathwayRampId from pathway where
@@ -61,14 +61,14 @@ getAnalyteFromPathway <- function(pathway,conpass=NULL,
   cid_list <- unlist(df2[,2])
   cid_list <- sapply(cid_list,shQuote)
   cid_list <- paste(cid_list,collapse = ",")
-
+  
   # Retrieve all common name from compounds associated with RaMP compound ids (query2)
   query3 <- paste0("select * from source where rampId in (",cid_list,");")
   con <- connectToRaMP(dbname=dbname,username=username,conpass=conpass,
                        host = host)
   df3 <- DBI::dbGetQuery(con,query3)
   DBI::dbDisconnect(con)
-
+  
   # Merge all of this together
   mdf1 <- merge(df3,df2,all.x=T)
   mdf1 <- merge(mdf1,df1,all.x=T,by="pathwayRampId")
@@ -76,54 +76,28 @@ getAnalyteFromPathway <- function(pathway,conpass=NULL,
   mdf1[,-which(colnames(mdf1) %in% "sourceId.y")]
   colnames(mdf1)[which(colnames(mdf1)=="sourceId.x")]="sourceId"
   mdf1$temp<-paste(mdf1$pathwayRampId,mdf1$rampId,sep=";")
-
-
+  # use regular expression to remove substring before ':'
+  mdf1$sourceId <- gsub('.*:','',mdf1$sourceId)
   out=data.frame(pathwayName=NA,pathwayCategory=NA,pathwayType=NA,compoundName=NA,
-	sourceCompoundIDs=NA,geneOrCompound=NA)
+                 sourceCompoundIDs=NA,geneOrCompound=NA)
   # Reformat so that you have one metabolite, with all synonyms, in one line:
   count=1
   allout=c()
   for (i in unique(mdf1$temp)) {
-	temp <- mdf1[which(mdf1$temp==i),]
-	out$sourceCompoundIDs <- paste(paste(temp$IDtype,temp$sourceId.x,sep=": "),collapse="; ")
-	out$pathwayName <- temp[1,"pathwayName"]
-	out$pathwayCategory <- temp[1,"pathwayCategory"]
-	out$pathwayType <- temp[1,"type"]
-	out$compoundName <- temp[1,"commonName"]
-	out$geneOrCompound <- temp[1,"geneOrCompound"]
-	count=count+1
-	allout=rbind(allout,out)
+    temp <- mdf1[which(mdf1$temp==i),]
+    out$sourceCompoundIDs <- paste(paste(temp$IDtype,temp$sourceId,sep=": "),collapse="; ")
+    out$pathwayName <- temp[1,"pathwayName"]
+    out$pathwayCategory <- temp[1,"pathwayCategory"]
+    out$pathwayType <- temp[1,"type"]
+    out$compoundName <- temp[1,"commonName"]
+    out$geneOrCompound <- temp[1,"geneOrCompound"]
+    count=count+1
+    allout=rbind(allout,out)
   }
-
+  
   print("Timing ..")
   print(proc.time() - now)
-
+  
   return(as.data.frame(allout))
 }
 
-
-#' Generate data.frame that has all search results from given files
-#'
-#' identifing the file type, then it returns table output to
-#' shiny renderTable function as preview of searching data
-#'
-#' @param infile a file object given from files
-#' @param conpass password for database access (string)
-#' @param dbname name of the mysql database (default is "ramp")
-#' @param username username for database access (default is "root")
-#' @param host host name for database access (default is "localhost")
-#' @return a data.frame either from multiple csv file
-#' or search through by a txt file.
-rampFastMetaFromPath_InputFile <- function(infile,conpass=NULL,
-	dbname="ramp",username="root",host = "localhost"){
-  name <- infile[[1,'name']]
-  summary <- data.frame(pathway  = character(0),id = character(0),
-                        source = character(0),metabolite = character(0))
-  rampOut <- list()
- for (i in 1:length(infile[,1])){
-     rampOut <- readLines(infile[[i,'datapath']])
-      rampOut <- readLines(infile)
-      summary <- getAnalyteFromPathway(rampOut,conpass=conpass,host = host)
-  }
-  return(summary)
-}
