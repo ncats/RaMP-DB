f53c6d45a5cb5d8a03106fae7ba31493909b7cb0
Bofei <8196386@qq.com>
2018-04-23 21:10:06 -0400

    update matrices and writingFunc

--- R/GeneralFunctions.R
+++ R/GeneralFunctions.R
@@ -9,3 +9,36 @@ loadOverlapMatrices<- function() {
   return(list(gene=gene_result,metab=metabolite_result,
 	analyte=analyte_result))
 }
+
+#' Update and save the overlap matrices based on current version of RaMP
+#' 
+#' @param method a string that specifies algorithm to compute overlap matrix
+#' should be 'balanced' or 'weighted'
+#' @param all a string that specifies which matrices to compute, should be in
+#' 'all','analyte'
+#' @param username a string that specifies name of MySQL database
+#' @param dbname a string that specifies database name of MySQL database
+#' @param conpass a string that specifies password for database connection
+#' @param host a string that specifes host for database connection
+#' @export
+updateOverlapMatrices <- function(method,all,
+                                  conpass,
+                                  host = 'localhost',dbname = 'ramp',
+                                  username = 'root'){
+  if(!(method %in% c('balanced','weighted'))){
+    stop('Wrong input for argument method')
+  }
+  if(!(all %in%c('all','metabolite','gene','analyte'))){
+    stop('Wrong input for argument all')
+  }
+  
+  if(all == 'all'){
+    result <- RaMP:::updateOverlapMatrix(min_analyte = 5,methods = 'balanced',together = T)
+    save(result[[1]],system.file(package = "RaMP",... = "extdata/metabolites_overlap_matrix.RData"))
+    save(result[[2]],system.file(package = "RaMP",... = "extdata/genes_overlap_matrix.RData"))
+  } else if(all == 'analyte'){
+    result <- RaMP:::updateOverlapMatrix(min_analyte = 5,methods = 'balanced',together = F)
+    save(result,system.file(package = "RaMP",... = "extdata/analytes_overlap_matrix.RData"))
+   
+  }
+}
