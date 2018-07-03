0b7126a95fd3b104ec77852b39569ae62a77e2fe
Rohith <vanam.7@osu.edu>
2018-07-03 15:36:43 -0400

    Created status-info boxes

--- inst/shinyApp/server/tab4.R
+++ inst/shinyApp/server/tab4.R
@@ -1,29 +1,70 @@
 dataInput_cata <- eventReactive(input$subText_cata,{
-  progress <- shiny::Progress$new()
+  tryCatch({
+    progress <- shiny::Progress$new()
+    
+    on.exit(progress$close())
+    
+    progress$set(message = "Querying databases based on catalyzation ...", value = 0)
+    progress$inc(0.3,detail = paste("Send Query ..."))
+    
+    # rampOut <- rampCataOut(input$KW_cata, 99999)
+    rampOut <- RaMP::rampFastCata(input$KW_cata,conpass=.conpass,host = .host,
+                                  dbname = .dbname, username = .username,
+                                  NameOrIds = input$CataInput_choices)
+    progress$inc(0.7,detail = paste("Done!"))
+    return (rampOut)
+  }, error = function(e) return())
   
-  on.exit(progress$close())
-  
-  progress$set(message = "Querying databases based on catalyzation ...", value = 0)
-  progress$inc(0.3,detail = paste("Send Query ..."))
-  
-  # rampOut <- rampCataOut(input$KW_cata, 99999)
-  rampOut <- RaMP::rampFastCata(input$KW_cata,conpass=.conpass,host = .host,
-				 dbname = .dbname, username = .username,
-                                 NameOrIds = input$CataInput_choices)
-  progress$inc(0.7,detail = paste("Done!"))
-  return (rampOut)
 })
 
+# summary_cata_out<- eventReactive(input$subText_cata,{
+#   if (!is.null(nrow(dataInput_cata()))){
+#     return (paste0("There are ",nrow(dataInput_cata())," analytes in the same reaction as input"))
+#   } else{
+#     return ("Input analyte is not involved in any reactions.")
+#   }
+# })
+# 
+# output$summary_cata <- renderText({
+#   summary_cata_out()
+# })
+
+#Summary
 summary_cata_out<- eventReactive(input$subText_cata,{
   if (!is.null(nrow(dataInput_cata()))){
     return (paste0("There are ",nrow(dataInput_cata())," analytes in the same reaction as input"))
-  } else{
-    return ("Input analyte is not involved in any reactions.")
+  } 
+})
+
+summary_cata_out_empty<- eventReactive(input$subText_cata,{
+  if (is.null(nrow(dataInput_cata()))){
+    return ("Input analyte is not involved in any reactions (or) Analyte name or Source-Id not found.")
   }
 })
 
 output$summary_cata <- renderText({
-  summary_cata_out()
+  if (!is.null(summary_cata_out())) {
+    return(summary_cata_out())
+  } else {
+    return(summary_cata_out_empty())
+  }
+})
+
+#status box
+output$statusbox_tab4_subtab1 <- shinydashboard::renderInfoBox({
+  if (!is.null(summary_cata_out())) {
+    shinydashboard::infoBox(
+      "Status",
+      HTML(paste("Successful")),
+      icon = icon("thumbs-up", lib = "glyphicon"),
+      color = "green", fill = TRUE)
+  } else if(!is.null(summary_cata_out_empty())) {
+    shinydashboard::infoBox(
+      "Status",
+      HTML(paste("Not-Found")),
+      icon = icon("thumbs-down", lib = "glyphicon"),
+      color = "yellow", fill = TRUE)
+  }
 })
 
 
@@ -77,11 +118,53 @@ observe({
 })
 
 data_mul_name_tab4 <- eventReactive(input$sub_mul_tab4,{
-  RaMP::rampFastCata(input$input_mul_tab4,conpass=.conpass,
-			dbname = .dbname, username = .username,
-                      host = .host,
-                      NameOrIds = input$input_mul_tab4_choices)
+  tryCatch({
+    RaMP::rampFastCata(input$input_mul_tab4,conpass=.conpass,
+                       dbname = .dbname, username = .username,
+                       host = .host,
+                       NameOrIds = input$input_mul_tab4_choices)
+  }, error = function(e) return())
+  
 })
+
+#Summary
+summary_cata_out_tab2<- eventReactive(input$sub_mul_tab4,{
+  if (!is.null(nrow(data_mul_name_tab4()))){
+    return ("Result Found")
+  } 
+})
+
+summary_cata_out_tab2_empty<- eventReactive(input$sub_mul_tab4,{
+  if (is.null(nrow(data_mul_name_tab4()))){
+    return ("Input analyte is not involved in any reactions (or) Analyte name or Source-Id not found.")
+  }
+})
+
+output$summary_cata_tab2 <- renderText({
+  if (!is.null(summary_cata_out_tab2())) {
+    return(summary_cata_out_tab2())
+  } else {
+    return(summary_cata_out_tab2_empty())
+  }
+})
+
+#status box
+output$statusbox_tab4_subtab2 <- shinydashboard::renderInfoBox({
+  if (!is.null(summary_cata_out_tab2())) {
+    shinydashboard::infoBox(
+      "Status",
+      HTML(paste("Successful")),
+      icon = icon("thumbs-up", lib = "glyphicon"),
+      color = "green", fill = TRUE)
+  } else if(!is.null(summary_cata_out_tab2_empty())) {
+    shinydashboard::infoBox(
+      "Status",
+      HTML(paste("Not-Found")),
+      icon = icon("thumbs-down", lib = "glyphicon"),
+      color = "yellow", fill = TRUE)
+  }
+})
+
 data_mul_file_tab4 <- eventReactive(input$sub_file_tab4,{
   infile <- input$inp_file_tab4
   print(paste0("Input file is ",infile))
