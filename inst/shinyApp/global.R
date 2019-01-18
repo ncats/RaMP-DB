2a1bb988d37e5d9c893d9112fa7c216b35586566
Rohith <vanam.7@osu.edu>
2019-01-18 13:49:03 -0500

    Fisher Test - Freq table sourced internally within package

--- inst/shinyApp/global.R
+++ inst/shinyApp/global.R
@@ -29,6 +29,4 @@ kw_pathway <- unname(unlist(unique(as.vector(DBI::dbGetQuery(con,'select pathway
 kw_analyte <- unname(unlist(unique(as.vector(DBI::dbGetQuery(con,'select Synonym from analytesynonym;')))))
 kw_source <- unname(unlist(unique(as.vector(DBI::dbGetQuery(con,'select sourceId from source;')))))
 
-load("FT_data.Rdata")
-
 DBI::dbDisconnect(con)
