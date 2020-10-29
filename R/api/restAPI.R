9e3b9dcb0a92adef423bf9b8b8b65058902e2bd2
Jorge Neyra <jorge.neyra@nih.gov>
2020-10-29 11:52:50 -0400

    added call to service data for graphics in front page

--- R/api/restAPI.R
+++ R/api/restAPI.R
@@ -12,6 +12,138 @@ cors <- function(res) {
     plumber::forward()
 }
 
+#* Return analyte source intersects
+#* @serializer unboxedJSON
+#* @get /api/analyte_intersects
+function() {
+
+    intersects <- list(
+        compounds=list(
+            list(
+                sets=list("KEGG"),
+                size=9
+            ),
+            list(
+                sets=list("REACTOME"),
+                size=226
+            ),
+            list(
+                sets=list("WP"),
+                size=631
+            ),
+            list(
+                sets=list("HMDB"),
+                size=106778
+            ),
+            list(
+                sets=list("KEGG", "REACTOME"),
+                size=20
+            ),
+            list(
+                sets=list("KEGG", "WP"),
+                size=125
+            ),
+            list(
+                sets=list("KEGG", "HMDB"),
+                size=3791
+            ),
+            list(
+                sets=list("REACTOME", "WP"),
+                size=817
+            ),
+            list(
+                sets=list("REACTOME", "HMDB"),
+                size=675
+            ),
+            list(
+                sets=list("WP", "HMDB"),
+                size=591
+            ),
+            list(
+                sets=list("KEGG", "REACTOME", "WP"),
+                size=142
+            ),
+            list(
+                sets=list("REACTOME", "WP", "HMDB"),
+                size=973
+            ),
+            list(
+                sets=list("KEGG", "REACTOME", "HMDB"),
+                size=940
+            ),
+            list(
+                sets=list("KEGG", "WP", "HMDB"),
+                size=19
+            ),
+            list(
+                sets=list("KEGG", "REACTOME", "WP", "HMDB"),
+                size=33
+            )
+        ),
+        genes=list(
+            list(
+                sets=list("KEGG"),
+                size=936
+            ),
+            list(
+                sets=list("REACTOME"),
+                size=278
+            ),
+            list(
+                sets=list("WP"),
+                size=975
+            ),
+            list(
+                sets=list("HMDB"),
+                size=1207
+            ),
+            list(
+                sets=list("KEGG", "REACTOME"),
+                size=171
+            ),
+            list(
+                sets=list("KEGG", "WP"),
+                size=1026
+            ),
+            list(
+                sets=list("KEGG", "HMDB"),
+                size=1781
+            ),
+            list(
+                sets=list("REACTOME", "WP"),
+                size=3825
+            ),
+            list(
+                sets=list("REACTOME", "HMDB"),
+                size=19
+            ),
+            list(
+                sets=list("WP", "HMDB"),
+                size=14
+            ),
+            list(
+                sets=list("KEGG", "REACTOME", "WP"),
+                size=4441
+            ),
+            list(
+                sets=list("REACTOME", "WP", "HMDB"),
+                size=1088
+            ),
+            list(
+                sets=list("KEGG", "REACTOME", "HMDB"),
+                size=28
+            ),
+            list(
+                sets=list("KEGG", "WP", "HMDB"),
+                size=21
+            ),
+            list(
+                sets=list("KEGG", "REACTOME", "WP", "HMDB"),
+                size=1544
+            )
+        )
+    )
+}
 
 #* Return analytes from pathway
 #* @param pathwaySourceId
