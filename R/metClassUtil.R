941eac7d5ef716b11997c32b2c1bd30cb5407b27
Tindall <tindallcm@ncats-2170685-m.myfiosgateway.com>
2021-11-16 15:05:52 -0500

    Added  metClassUtil.R

---
+++ R/metClassUtil.R
@@ -0,0 +1,85 @@
+#' Query to retrieve database ID prefixes for analyte types
+#' @return Returns list of database ID prefixes for selected 'gene' or 'compound'
+#' @export  getPrefixesFromAnalytes
+
+getPrefixesFromAnalytes<-function(analyteType="gene") {
+  con <- connectToRaMP()
+  if (analyteType=="gene"){
+    query1 <- "select distinct(IDtype) from source where geneOrCompound ='gene';"
+    df1<- RMariaDB::dbGetQuery(con,query1)
+    }
+  else if (analyteType=="compound"){
+    query2 <- "select distinct(IDtype) from source where geneOrCompound ='compound';"
+    df1 <- RMariaDB::dbGetQuery(con,query2)
+    }
+  else{
+    print("analyteType must be 'gene' or 'compound'")
+  }
+  RMariaDB::dbDisconnect(con)
+  return(df1[,1])
+}
+
+
+#' Returns class data sources for metabolites
+#' @return Returns list of data sources for metabolites
+
+getMetabClassDataSources<-function(){
+  con <- connectToRaMP()
+  query1<-"select distinct(source) from metabolite_class order by source asc"
+  results<- RMariaDB::dbGetQuery(con,query1)
+  return(results)
+}
+
+
+#' Returns class categories for metabolites
+#' @return Returns metabolite class types
+#' @export getMetabClassTypes
+getMetabClassTypes<-function(){
+  con <- connectToRaMP()
+  query1<-"select distinct(class_level_name) from metabolite_class order by class_level_name asc"
+  results<- RMariaDB::dbGetQuery(con,query1)
+  return(results)
+  }
+#` Returns chemical classes for classType
+#' @param classType one of the metab class types as returned by getMetabClassTypes() function; if null
+#' will return a list of available classes for each class type
+#' @return Returns metabolite classes for classTypes
+#' @export getMetabChemClass
+
+
+getMetabChemClass<-function(classType= 'ClassyFire_super_class') {
+  if (!is.null(classType)) {
+    con <- connectToRaMP()
+    query1<- paste0("select class_level_name, class_name from metabolite_class where class_level_name = '",classType,"' group by class_level_name, class_name")
+    res<-RMariaDB::dbGetQuery(con,query1)
+    res<-split(res$class_name,res$class_level_name)
+  }
+
+  else if (is.null(classType)){
+    con <- connectToRaMP()
+    query1<- "select class_level_name, class_name from metabolite_class group by class_level_name, class_name"
+    res<-RMariaDB::dbGetQuery(con,query1)
+    res<-split(res$class_name,res$class_level_name)
+  }
+
+  if (length(res)==0){
+      message(paste0(classType, ' Not Defined. Available Class Types:'))
+      res<-getMetabClassTypes()
+
+  }
+  return(res)
+  }
+
+
+  #' Returns list of available ontologies from database
+  #' @return Returns ontologies listed in database
+  #' @export getOntologies
+
+getOntologies<-function(){
+    con <- connectToRaMP()
+    query1<- "select distinct(commonName) from ontology"
+    OntoRes<-RMariaDB::dbGetQuery(con,query1)
+    return(OntoRes)
+}
+
+
