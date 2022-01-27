46b7535bfa29d131df8f624a4ad730042913e28c
Braisted <braistedjc@nih.gov>
2022-01-27 13:03:16 -0500

    getMetaFromOntolV2() test method

--- R/OntologyQueryFunctions.R
+++ R/OntologyQueryFunctions.R
@@ -6,7 +6,7 @@
 #'
 #' @examples
 #' \dontrun{
-#' pkg.globals <- setConnectionToRaMP(dbname = "ramp2", username = "root", 
+#' pkg.globals <- setConnectionToRaMP(dbname = "ramp2", username = "root",
 #' 	conpass = "", host = "localhost")
 #' getOntoFromMeta("hmdb:HMDB0071437")
 #' }
@@ -96,7 +96,7 @@ getOntoFromMeta <- function(analytes, NameOrIds = "ids") {
 #' @return dataframe that  contains searched analytes from given ontology
 #' @examples
 #' \dontrun{
-#' pkg.globals <- setConnectionToRaMP(dbname = "ramp2", username = "root", 
+#' pkg.globals <- setConnectionToRaMP(dbname = "ramp2", username = "root",
 #' 	conpass = "", host = "localhost")
 #' getMetaFromOnto("Adiposome")
 #' }
@@ -233,15 +233,86 @@ getMetaFromOnto <- function(ontology) {
       mdf_final <- do.call(rbind.data.frame, mdf_final)
       mdf_final <- mdf_final[c("Metabolites", "sourceId", "Ontology", "HMDBOntologyType")]
       mdf_final <- unique(mdf_final)
-    # Collapse rows for each metabolite:
-    # Metabolites<-c()
-    # print("NO ERROR YET")
-    # print(dim(mdf))
-    # out=data.table::setDT(mdf)[,lapply(data.table::.SD, function(x) paste0(unique(x),collapse=", ")),
-                                        # 	by=Metabolites]
+
+
     return(mdf_final)
   } else {
     warning("Ontology not found in RaMP.  Run the getOntologies() function to see available ontologies.")
     return(NA)
   }
 }
+
+#' function that query database to find analytes in given ontologies
+#' @param ontology a vector of ontology or ontologies delimited by new line character
+#'
+#' @return dataframe that  contains searched analytes from given ontology
+#' @examples
+#' \dontrun{
+#' pkg.globals <- setConnectionToRaMP(dbname = "ramp2", username = "root",
+#' 	conpass = "", host = "localhost")
+#' getMetaFromOnto("Adiposome")
+#' }
+#' @importFrom rlang .data
+#' @export
+getMetaFromOntoV2 <- function(ontology) {
+
+  print("Retreiving Metabolites for input ontology terms.")
+  now <- proc.time()
+  if(is.character(ontology)){
+    if(grepl("\n",ontology)[1]){
+      list_ontology <- strsplit(ontology,"\n")
+      list_ontology <- unlist(list_ontology)
+    } else if(grepl(",",ontology)[1]){
+      list_ontology <- strsplit(ontology,",")
+      list_ontology <- unlist(list_ontology)
+    } else {
+      list_ontology <- ontology
+    }
+  } else if(is.data.frame(ontology)){
+    list_ontology <- unlist(ontology)
+  }
+
+  list_ontology <- unique(list_ontology)
+
+  # Find the ontology in ramp2
+  allontos <- getOntologies()
+  matched_ontos <- unlist(lapply(list_ontology,
+                                 function(x) grep(paste0("^",x,"$"), allontos$commonName)))
+
+  # Only proceed if df has anything returned
+  if (length(matched_ontos) > 0) {
+
+    print(paste0("Found ",length(matched_ontos)," ontology term matches."))
+
+    ontologyList <- paste0("'", paste(list_ontology, collapse="','"),"'")
+
+    sql = paste0("select rampId,
+          group_concat(distinct s.sourceId order by s.sourceId separator '; ') as source_ids,
+          group_concat(distinct s.commonName order by s.commonName separator '; ') as common_names, o.commonName, o.HMDBOntologyType
+          from source s, analytehasontology ao, ontology o where ao.rampOntologyId in (
+          select distinct rampOntologyId from ontology where commonName in (",ontologyList,"))
+          and o.rampOntologyId = ao.rampOntologyId and s.rampId = ao.rampCompoundId
+          group by o.commonName, s.rampId")
+
+    con <- connectToRaMP()
+    mdf_final <- RMariaDB::dbGetQuery(con,sql)
+    RMariaDB::dbDisconnect(con)
+    mdf_final <- unique(mdf_final)
+    mdf_final <- mdf_final[,c(4,5,3,2)]
+    colnames(mdf_final) <- c('ontologyTerm', 'ontologyCategory', 'metNames', 'metIds')
+
+    print(paste0("Found ",nrow(mdf_final)," metabolites associated with the input ontology terms."))
+    print("Finished getting metabolies from ontology terms.")
+
+    return(mdf_final)
+  } else {
+    warning("The input ontology terms were not found in RaMP.\nRun the getOntologies() function to see available ontology terms.")
+    return(NA)
+  }
+}
+
+
+
+
+
+
