fef5912146bfe410297e43652ced73f81021251c
johnbraisted <jb212828@gmail.com>
2022-03-03 12:12:27 -0500

    new mysql dump (new indices), update rampFastCata() method returning input analyte

--- R/ReturnGeneMetab_SameRxn.R
+++ R/ReturnGeneMetab_SameRxn.R
@@ -15,6 +15,154 @@
 #' }
 #' @export
 rampFastCata <- function(analytes="none", NameOrIds="ids") {
+  if(length(analytes)==1){
+    if(analytes=="none"){
+      stop("Please provide input analytes")}}
+  if (!(NameOrIds %in% c('names','ids')))
+    stop('Please specify search by "names" or "ids"')
+
+  now <- proc.time()
+  if(is.character(analytes)){
+    if(grepl("\n",analytes)[1]){
+      list_metabolite <- strsplit(analytes,"\n")
+      list_metabolite <- unlist(list_metabolite)
+    } else if(grepl(",",analytes)[1]){
+      list_metabolite <- strsplit(analytes,"\n")
+      list_metabolite <- unlist(list_metabolite)
+    } else {
+      list_metabolite <- analytes
+    }
+  } else if(is.data.frame(analytes)){
+    list_metabolite <- unlist(analytes)
+  } else {stop("The input 'analytes' is not a recognized format. Please check input.")}
+
+  list_metabolite <- unique(list_metabolite)
+  list_metabolite <- sapply(list_metabolite,shQuote)
+  list_metabolite <- paste(list_metabolite,collapse = ",")
+
+  #print(list_metabolite)
+
+  con <- connectToRaMP()
+
+  if(NameOrIds == 'ids') {
+
+    print("Analyte ID-based reaction partner query.")
+
+    metQuery <- paste0("select c.sourceId as input_analyte, group_concat(distinct c.commonName order by c.commonName asc separator '; ') as input_common_names,
+  group_concat(distinct g.commonName order by g.commonName asc separator '; ') as rxn_partner_common_name,
+  group_concat(distinct g.sourceId order by g.sourceId asc separator '; ') as rxn_partner_ids,
+  g.rampId from catalyzed r
+  join source g on r.rampGeneId = g.rampId
+  join source c on r.rampCompoundId = c.rampId
+  where c.sourceId in (",list_metabolite,") group by g.rampId, c.sourceId")
+
+    print("Building metabolite to gene relations.")
+
+    df1 <- RMariaDB::dbGetQuery(con, metQuery)
+
+    print(paste0("Number of met2gene relations: ",(nrow(df1))))
+
+    geneQuery <- paste0("select g.sourceId as input_analyte, group_concat(distinct g.commonName order by g.commonName asc separator '; ') as input_common_names,
+  group_concat(distinct c.commonName order by c.commonName asc separator '; ') as rxn_partner_common_name,
+  group_concat(distinct c.sourceId order by c.sourceId asc separator '; ') as rxn_partner_ids,
+  c.rampId from catalyzed r
+  join source g on r.rampGeneId = g.rampId
+  join source c on r.rampCompoundId = c.rampId
+  where g.sourceId in (", list_metabolite,") group by c.rampId, g.sourceId")
+
+    print("Building gene to metabolite relations.")
+
+    df2 <- RMariaDB::dbGetQuery(con, geneQuery)
+  } else {
+
+    # working on 'names' query
+    # note that we now bring in the synonyms table
+
+    print("Analyte name-based reaction partner query.")
+
+    metQuery <- paste0("select s.Synonym as input_analyte, group_concat(distinct c.commonName order by c.commonName asc separator '; ') as input_common_names,
+  group_concat(distinct g.commonName order by g.commonName asc separator '; ') as rxn_partner_common_name,
+  group_concat(distinct g.sourceId order by g.sourceId asc separator '; ') as rxn_partner_ids,
+  g.rampId from catalyzed r
+  join source g on r.rampGeneId = g.rampId
+  join source c on r.rampCompoundId = c.rampId
+  join analytesynonym s on s.rampId = r.rampCompoundId
+  where s.Synonym in (",list_metabolite,") group by g.rampId, s.Synonym")
+
+    print("Building metabolite to gene relations.")
+
+    df1 <- RMariaDB::dbGetQuery(con, metQuery)
+
+    print(paste0("Number of met2gene relations: ",(nrow(df1))))
+
+    geneQuery <- paste0("select s.Synonym as input_analyte, group_concat(distinct g.commonName order by g.commonName asc separator '; ') as input_common_names,
+  group_concat(distinct c.commonName order by c.commonName asc separator '; ') as rxn_partner_common_name,
+  group_concat(distinct c.sourceId order by c.sourceId asc separator '; ') as rxn_partner_ids,
+  c.rampId from catalyzed r
+  join source g on r.rampGeneId = g.rampId
+  join source c on r.rampCompoundId = c.rampId
+  join analytesynonym s on s.rampId = r.rampGeneId
+  where s.Synonym in (", list_metabolite,") group by c.rampId, s.Synonym")
+
+    print("Building gene to metabolite relations.")
+
+    df2 <- RMariaDB::dbGetQuery(con, geneQuery)
+
+    print(paste0("Number of gene2met relations: ",(nrow(df2))))
+  }
+
+
+  RMariaDB::dbDisconnect(con)
+
+  if(!is.null(df1) && nrow(df1) > 0) {
+    df1$query_relation <- 'met2gene'
+    result <- df1
+    if(!is.null(df2) && nrow(df2) > 0) {
+      df2$query_relation <- 'gene2met'
+      result <- rbind(result, df2)
+    }
+  } else {
+    if(!is.null(df2) && nrow(df2) > 0) {
+      df2$query_relation <- 'gene2met'
+      result <- df2
+    } else {
+      result <- NULL
+    }
+  }
+
+  if(!is.null(result)) {
+    # remove rampId column
+    result <- subset(result, select=-c(rampId))
+    # move relation first
+    result <- result[,c(ncol(result), 1:(ncol(result)-1))]
+  }
+
+  print(paste0("Total Relation Count: ", (nrow(result))))
+
+  return(result)
+}
+
+
+
+
+
+
+#' Retrieves analytes that involved in same reaction as input metabolite
+#'
+#' @param analytes a vector of analytes that need to be searched
+#' @param NameOrIds whether input is "names" or "ids" (default is "ids")
+#' @return a dataframe containing query results. If the input is a metabolite, the function will output
+#' gene transcript common names and source IDs that are known to catalyze
+#' reactions in the same pathway as that metabolite. Conversely, if the input
+#' is a gene, the function will return the common name and source id of metabolites
+#' known to be catalyzed directly or indirectly by that gene.
+#'
+#' @examples
+#' \dontrun{
+#' pkg.globals <- setConnectionToRaMP(dbname="ramp2",username="root",conpass="",host = "localhost")
+#' rampFastCata(analytes="creatine",NameOrIds="names")
+#' }
+rampFastCataOriginal <- function(analytes="none", NameOrIds="ids") {
     if(length(analytes)==1){
         if(analytes=="none"){
     stop("Please provide input analytes")}}
