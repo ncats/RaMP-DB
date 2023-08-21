846ead3f3fd00929c6be241d1d8743f2cceba18f
johnbraisted <jb212828@gmail.com>
2023-08-21 13:46:49 -0400

    save the setupRdata DB query function

--- R/dbWorker.R
+++ R/dbWorker.R
@@ -69,6 +69,8 @@ setupRdata <- function() {
 
     for(i in 1:nrow(objs)) {
       varName = objs[i,1]
+      print("Initializing var")
+      print(varName)
       blob = objs[i,2]
       blob = blob[[1]]
       obj = memDecompress(from=blob, type = 'gzip', asChar = T)
