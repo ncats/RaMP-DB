a30224d443f17dd3d33ca765e4449d2fabbcae4e
jorainer <johannes.rainer@gmail.com>
2023-08-23 08:21:33 +0200

    refactor: add RaMP class and utilities to download dbs
    - Add the `RaMP` class and constructor function.
    - Add `listRaMPVersions` function.
    - Add utility functions to cache and manage RaMP databases.

--- R/zzz.R
+++ R/zzz.R
@@ -4,27 +4,15 @@
 
 .onAttach <- function(libname, pkgname) {
 
-  print(paste0("Loading Ramp version: ", packageDescription("RaMP")$Version))
-  print("Checking for existing BiocFileCache entry for the RaMP Database.")
-
-  bfc <- BiocFileCache::BiocFileCache(cache = BiocFileCache::getBFCOption("CACHE"), ask=F)
-  cacheInfo <- BiocFileCache::bfcinfo()
-  cacheInfo <- cacheInfo[grepl("RaMP", cacheInfo$rname),]
-
-  if(nrow(cacheInfo) < 1) {
-    print("")
-    print("RaMP Database is not in file cache. Performing one-time download.")
-    url = packageDescription("RaMP")$Config_ramp_db_url
-    print("One time retrieval of RaMP Database Cache. This will take about 1 minute to download and unzip.")
-    path <- BiocFileCache::bfcadd(bfc, url, fname='exact')
-    cid <- names(path)
-    R.utils::gunzip(path, remove=F)
-    newpath <- gsub(".gz", "", path)
-    BiocFileCache::bfcremove(bfc, cid)
-    BiocFileCache::bfcadd(bfc, newpath, fname='exact')
-  } else {
-    print(paste0("RaMP DB found in BiocFileCache, SQLite File: ",cacheInfo$rpath[1]))
-  }
+    message("Loading Ramp version: ", packageDescription("RaMP")$Version)
+    db_local <- listRaMPVersions(local = TRUE)
+    if (length(db_local)) {
+        message("Locally cached RaMP Database versions:")
+        message(paste0(" - ", db_local, "\n"))
+    } else {
+        message("No cached RaMP databases present. Use 'RaMP()' to download ",
+                "the most recent release. See '?RaMP' for more information.")
+    }
 }
 
 
