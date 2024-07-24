8dca5a021b7bfd1b2f56605b63cd99de18dc0aa0
Tisch <tischam@nih.gov>
2024-07-24 09:46:58 -0400

    example formatting

--- R/rampChemClassQueries.R
+++ R/rampChemClassQueries.R
@@ -42,7 +42,14 @@
 #'@examples
 #'\dontrun{
 #' # metabolite list of interest
-#' metabolites.of.interest = c("pubchem:64969", "chebi:16958", "chemspider:20549", "kegg:C05598", "chemspider:388809", "pubchem:53861142", "hmdb:HMDB0001138", "hmdb:HMDB0029412")
+#' metabolites.of.interest = c("pubchem:64969",
+#'                              "chebi:16958",
+#'                              "chemspider:20549",
+#'                              "kegg:C05598",
+#'                              "chemspider:388809",
+#'                              "pubchem:53861142",
+#'                              "hmdb:HMDB0001138",
+#'                              "hmdb:HMDB0029412")
 #'
 #' metClassResult <- chemicalClassSurvey(mets = metabolites.of.interest)
 #'
@@ -199,7 +206,14 @@ chemicalClassSurvey <- function(db = RaMP(), mets, background = "database", back
 #'@examples
 #'\dontrun{
 #' # metabolite list of interest
-#' metabolites.of.interest = c("pubchem:64969", "chebi:16958", "chemspider:20549", "kegg:C05598", "chemspider:388809", "pubchem:53861142", "hmdb:HMDB0001138", "hmdb:HMDB0029412")
+#' metabolites.of.interest = c("pubchem:64969",
+#'                             "chebi:16958",
+#'                             "chemspider:20549",
+#'                             "kegg:C05598",
+#'                             "chemspider:388809",
+#'                             "pubchem:53861142",
+#'                             "hmdb:HMDB0001138",
+#'                             "hmdb:HMDB0029412")
 #'
 #' chemical.enrichment <- chemicalClassEnrichment(db = rampDB, mets = metabolites.of.interest)
 #'}
